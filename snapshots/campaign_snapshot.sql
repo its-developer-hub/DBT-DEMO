6022bee085035229e56fd6786d367cf6253ff7dc
Velavan Dhanavel <getitrajesh@rediffmail.com>
2025-07-27 12:17:19 +0000

    Added Snapshot for campaigns table

---
+++ snapshots/campaign_snapshot.sql
@@ -0,0 +1,16 @@
+{% snapshot campaign_snapshot %}
+
+{{
+
+    config(
+      target_schema = 'hist_stage',
+      unique_key = 'campaign_id',
+      strategy = 'timestamp',
+      updated_at = 'updated_ts'
+    )
+}}
+
+
+select * from {{ souce('campaign','campaigns') }}
+
+{% endsnapshot %}
