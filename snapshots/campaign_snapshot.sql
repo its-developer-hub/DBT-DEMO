2a849a08021fe356c90fb5288e53398945705bbc
Velavan Dhanavel <getitrajesh@rediffmail.com>
2025-07-28 11:42:00 +0000

    Updated snapshot

--- snapshots/campaign_snapshot.sql
+++ snapshots/campaign_snapshot.sql
@@ -8,10 +8,11 @@
       strategy = 'timestamp',
       updated_at = 'updated_ts',
       dbt_valid_to_current = "to_date('9999-12-31')",
-
+      hard_deletes = 'new_record',
+      
       snapshot_meta_column_names={
-              dbt_valid_from: 'effective_from',
-              dbt_valid_to: 'effective_to'
+              dbt_valid_from: effective_from,
+              dbt_valid_to: effective_to
       }      
     )
 }}
